350fe5bdbed45eba7b43eabbe158e1d778a26b22
NightmareEDM <lillykavsek@hotmail.com>
2016-04-04 14:10:19 +0200

    optimisation aufg2

--- aufgabe1_kavsek/main.js
+++ aufgabe1_kavsek/main.js
@@ -1,20 +1,16 @@
 console.log("Aufgabe 1 & 2 - Lilly Kavsek\n");
-var line = "-------------------";
 
-// activate fs & calc & lodash
+// dependencies
 var fs = require('fs');
 var chalk = require('chalk');
-var lodash = require('lodash');
 
-// read
+// read and print
 fs.readFile(__dirname + "/wolkenkratzer.json", function (err, data) {
     data = data.toString();
     jsondata = JSON.parse(data);
+
     for (var counter = 0; counter < jsondata.wolkenkratzer.length; counter++) {
-
-        // print one tower
         console.log("Name:  " + chalk.blue(jsondata.wolkenkratzer[counter].name) + "\nStadt: " + chalk.green(jsondata.wolkenkratzer[counter].stadt) + "\nHöhe:  " + chalk.red(jsondata.wolkenkratzer[counter].hoehe) + "m");
-        console.log(line);
-
+        console.log("-------------------");
     }
 });
