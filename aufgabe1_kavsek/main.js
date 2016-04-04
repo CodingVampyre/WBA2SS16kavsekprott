f1282a160b7f47d3e6a4cfb775e6e074efead9d0
NightmareEDM <lillykavsek@hotmail.com>
2016-04-04 12:56:14 +0200

    kavsek solved aufgabe2

--- aufgabe1_kavsek/main.js
+++ aufgabe1_kavsek/main.js
@@ -1,15 +1,25 @@
-console.log("Aufgabe 1 - Lilly Kavsek");
+console.log("Aufgabe 1 & 2 - Lilly Kavsek\n");
 
 var line = "-------------------";
 var counter = 0;
 
 // activate fs
 var fs = require('fs');
+
+// activate chalk
+var chalk = require('chalk');
+
+// read
 fs.readFile(__dirname + "/wolkenkratzer.json", function (err, data) {
     data = data.toString();
     jsondata = JSON.parse(data);
     for (var counter = 0; counter < jsondata.wolkenkratzer.length; counter++) {
-        console.log("Name:  " + jsondata.wolkenkratzer[counter].name + "\nStadt: " + jsondata.wolkenkratzer[counter].stadt + "\nHöhe:  " + jsondata.wolkenkratzer[counter].hoehe + "m");
+
+        // print one tower
+        console.log("Name:  " + chalk.blue(jsondata.wolkenkratzer[counter].name) + "\nStadt: " +
+            chalk.green(jsondata.wolkenkratzer[counter].stadt) + "\nHöhe:  " +
+            chalk.red(jsondata.wolkenkratzer[counter].hoehe) + "m");
         console.log(line);
+
     }
 });
