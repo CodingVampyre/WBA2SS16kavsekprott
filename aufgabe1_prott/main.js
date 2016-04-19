361d8c20bb99e8016c275dcd62407a594704ae34
tim <tim.prott@fh-koeln.de>
2016-04-19 14:24:35 +0200

    tim aufgabe3 wip

--- aufgabe1_prott/main.js
+++ aufgabe1_prott/main.js
@@ -5,9 +5,25 @@ var fs = require('fs');
 var utf8 = require('utf8');
 var chalk = require('chalk');
 
+function sortHeight(a, b) {
+    if (a.hoehe < b.hoehe) {
+        return -1;
+    } else if (a.hoehe > b.hoehe) {
+        return 1;
+    }
+    return 0;
+}
+
 fs.readFile(__dirname + "/wolkenkratzer.json", function (err, data) {
     data = data.toString();
     data = JSON.parse(data);
+    data = data.wolkenkratzer.sort(sortHeight);
+
+    // write to file
+    var jsondataJ = JSON.stringify(data);
+    fs.writeFile(__dirname + "/wolkenkratzer_sortiert.json", jsondataJ, function (err) {
+        console.log("wrote successfully");
+    });
 
     for (var i = 0; i < data.wolkenkratzer.length; i++) {
 
