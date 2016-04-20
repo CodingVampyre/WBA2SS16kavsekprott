be8b851922f8deabc9318dcb49122edfb8223d4b
NightmareEDM <lillykavsek@hotmail.com>
2016-04-20 14:59:38 +0200

    added create.sql

---
+++ restaumania/db/creation.sql
@@ -0,0 +1,129 @@
+
+CREATE TABLE card_subcatergory (
+                subc_id INT AUTO_INCREMENT NOT NULL,
+                subc_name VARCHAR NOT NULL,
+                description VARCHAR NOT NULL,
+                PRIMARY KEY (subc_id)
+);
+
+
+CREATE TABLE rankings_enum (
+                ranking_id INT AUTO_INCREMENT NOT NULL,
+                ranking_name VARCHAR NOT NULL,
+                PRIMARY KEY (ranking_id)
+);
+
+
+CREATE TABLE user_rights (
+                right_level INT AUTO_INCREMENT NOT NULL,
+                right_name VARCHAR NOT NULL,
+                PRIMARY KEY (right_level)
+);
+
+
+CREATE TABLE card_category (
+                cat_id INT AUTO_INCREMENT NOT NULL,
+                cat_name VARCHAR NOT NULL,
+                description VARCHAR NOT NULL,
+                PRIMARY KEY (cat_id)
+);
+
+
+CREATE TABLE restaurant_user (
+                user_id INT AUTO_INCREMENT NOT NULL,
+                nickname VARCHAR NOT NULL,
+                PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE user_rights_assignment (
+                right_level INT NOT NULL,
+                user_id INT NOT NULL,
+                PRIMARY KEY (right_level, user_id)
+);
+
+
+CREATE TABLE restaurant (
+                restaurant_id INT AUTO_INCREMENT NOT NULL,
+                restaurant_name VARCHAR(128) NOT NULL,
+                restaurant_description VARCHAR NOT NULL,
+                user_id INT NOT NULL,
+                creation_timestamp DATETIME DEFAULT NOW() NOT NULL,
+                PRIMARY KEY (restaurant_id)
+);
+
+
+CREATE TABLE visitors (
+                restaurant_id INT NOT NULL,
+                user_id INT NOT NULL,
+                experience VARCHAR,
+                ranking_id INT NOT NULL,
+                PRIMARY KEY (restaurant_id, user_id)
+);
+
+
+CREATE TABLE card_entry (
+                entry_id INT AUTO_INCREMENT NOT NULL,
+                restaurant_id INT NOT NULL,
+                cat_id INT NOT NULL,
+                subc_id INT NOT NULL,
+                entry_name VARCHAR NOT NULL,
+                entry_details VARCHAR NOT NULL,
+                price DOUBLE PRECISIONS DEFAULT 0.00,
+                PRIMARY KEY (entry_id)
+);
+
+
+ALTER TABLE card_entry ADD CONSTRAINT card_subcatergory_card_entry_fk
+FOREIGN KEY (subc_id)
+REFERENCES card_subcatergory (subc_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE visitors ADD CONSTRAINT rankings_enum_visitors_fk
+FOREIGN KEY (ranking_id)
+REFERENCES rankings_enum (ranking_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE user_rights_assignment ADD CONSTRAINT user_rights_user_rights_assignment_fk
+FOREIGN KEY (right_level)
+REFERENCES user_rights (right_level)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE card_entry ADD CONSTRAINT card_category_card_entry_fk
+FOREIGN KEY (cat_id)
+REFERENCES card_category (cat_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE restaurant ADD CONSTRAINT restaurant_user_restaurant_fk
+FOREIGN KEY (user_id)
+REFERENCES restaurant_user (user_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE visitors ADD CONSTRAINT restaurant_user_visitors_fk
+FOREIGN KEY (user_id)
+REFERENCES restaurant_user (user_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE user_rights_assignment ADD CONSTRAINT restaurant_user_user_rights_assignment_fk
+FOREIGN KEY (user_id)
+REFERENCES restaurant_user (user_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE card_entry ADD CONSTRAINT restaurant_card_entry_fk
+FOREIGN KEY (restaurant_id)
+REFERENCES restaurant (restaurant_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE visitors ADD CONSTRAINT restaurant_visitors_fk
+FOREIGN KEY (restaurant_id)
+REFERENCES restaurant (restaurant_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
