8a09ac9a8b560e751fe667bff6fd0d29ae563bf8
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-20 23:19:26 +0200

    update creation.sql

--- restaumania/db/creation.sql
+++ restaumania/db/creation.sql
@@ -1,36 +1,40 @@
+DROP DATABASE restaumania;
+CREATE DATABASE restaumania;
+USE restaumania;
+
 CREATE TABLE card_subcatergory (
                 subc_id INT AUTO_INCREMENT NOT NULL,
-                subc_name VARCHAR NOT NULL,
-                cat_description VARCHAR NOT NULL,
+                subc_name VARCHAR(32) NOT NULL,
+                cat_description VARCHAR(256) NOT NULL,
                 PRIMARY KEY (subc_id)
 );
 
 
 CREATE TABLE rankings_enum (
                 ranking_id INT AUTO_INCREMENT NOT NULL,
-                ranking_name VARCHAR NOT NULL,
+                ranking_name VARCHAR(32) NOT NULL,
                 PRIMARY KEY (ranking_id)
 );
 
 
 CREATE TABLE user_rights (
                 right_level INT AUTO_INCREMENT NOT NULL,
-                right_name VARCHAR NOT NULL,
+                right_name VARCHAR(32) NOT NULL,
                 PRIMARY KEY (right_level)
 );
 
 
 CREATE TABLE card_category (
                 cat_id INT AUTO_INCREMENT NOT NULL,
-                cat_name VARCHAR NOT NULL,
-                description VARCHAR NOT NULL,
+                cat_name VARCHAR(32) NOT NULL,
+                description VARCHAR(256) NOT NULL,
                 PRIMARY KEY (cat_id)
 );
 
 
 CREATE TABLE restaurant_user (
                 user_id INT AUTO_INCREMENT NOT NULL,
-                nickname VARCHAR NOT NULL,
+                nickname VARCHAR(32) NOT NULL,
                 PRIMARY KEY (user_id)
 );
 
@@ -45,9 +49,9 @@ CREATE TABLE user_rights_assignment (
 CREATE TABLE restaurant (
                 restaurant_id INT AUTO_INCREMENT NOT NULL,
                 restaurant_name VARCHAR(128) NOT NULL,
-                restaurant_description VARCHAR NOT NULL,
+                restaurant_description VARCHAR(1024) NOT NULL,
                 user_id INT NOT NULL,
-                creation_timestamp DATETIME DEFAULT NOW() NOT NULL,
+                creation_timestamp TIMESTAMP DEFAULT NOW() NOT NULL,
                 PRIMARY KEY (restaurant_id)
 );
 
@@ -55,7 +59,7 @@ CREATE TABLE restaurant (
 CREATE TABLE visitors (
                 restaurant_id INT NOT NULL,
                 user_id INT NOT NULL,
-                experience VARCHAR,
+                experience VARCHAR(1024),
                 ranking_id INT NOT NULL,
                 PRIMARY KEY (restaurant_id, user_id)
 );
@@ -66,9 +70,9 @@ CREATE TABLE card_entry (
                 restaurant_id INT NOT NULL,
                 cat_id INT NOT NULL,
                 subc_id INT NOT NULL,
-                entry_name VARCHAR NOT NULL,
-                entry_details VARCHAR NOT NULL,
-                price DOUBLE PRECISIONS DEFAULT 0.00,
+                entry_name VARCHAR(32) NOT NULL,
+                entry_details VARCHAR(256) NOT NULL,
+                price FLOAT DEFAULT 0.0,
                 PRIMARY KEY (entry_id)
 );
 
