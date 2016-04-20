54f2f8e815b703cacfd225bbace6c362944bbe54
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-20 22:49:07 +0200

    added install_requirements.sh

---
+++ restaumania/install_requirements.sh
@@ -0,0 +1,9 @@
+echo "Welcome to restaumania-setup!"
+
+sudo apt-get install python2.7
+sudo apt-get install mysql-db mysql-client
+sudo apt-get install python-pip
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-dev
+
+sudo pip install MySql-python
