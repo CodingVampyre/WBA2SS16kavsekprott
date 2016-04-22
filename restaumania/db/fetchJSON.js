550b071e5295717d203f3a963ff596ba2acef02b
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-22 14:35:08 +0200

    update

--- restaumania/db/fetchJSON.js
+++ restaumania/db/fetchJSON.js
@@ -1,10 +1,11 @@
-var fs = require('fs')
+var fs = require('fs');
+var chalk = require('chalk');
 
 fs.readFile(__dirname + "/restList.json", function (err, data){
 	data = data.toString();
 	data = JSON.parse(data);
 
 	for(var i=0; i<data.length; i++){
-		console.log(data[i].rname + ": " + data[i].rdesc);
+		console.log(chalk.red(data[i].rname) + ": " + data[i].rdesc);
 	}
 });
