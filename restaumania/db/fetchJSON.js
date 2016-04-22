ac0e618f8fa14aa707cd3d37c3a618e597e08ab7
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-22 15:10:54 +0200

    update getrestaurant.py

--- restaumania/db/fetchJSON.js
+++ restaumania/db/fetchJSON.js
@@ -6,6 +6,6 @@ fs.readFile(__dirname + "/restList.json", function (err, data){
 	data = JSON.parse(data);
 
 	for(var i=0; i<data.length; i++){
-		console.log(chalk.red(data[i].rname) + ": " + data[i].rdesc);
+		console.log(chalk.red(data[i].rname) + " (" + chalk.yellow(data[i].resid) + "): " + data[i].rdesc);
 	}
 });
--- restaumania/db/getRestaurantList.py
+++ restaumania/db/getRestaurantList.py
@@ -15,14 +15,15 @@ except:
 	sys.exit(-1)
 
 cursor = db.cursor()
-cursor.execute("SELECT restaurant_name, restaurant_description FROM restaurant")
+cursor.execute("SELECT restaurant_id, restaurant_name, restaurant_description FROM restaurant")
 data = cursor.fetchall()
 
 object_list = []
 for single in data:
 	d = collections.OrderedDict()
-	d['rname'] = unicode(single[0], 'ISO-8859-1')
-	d['rdesc'] = unicode(single[1], 'ISO-8859-1')
+	d['resid'] = single[0]
+	d['rname'] = unicode(single[1], 'ISO-8859-1')
+	d['rdesc'] = unicode(single[2], 'ISO-8859-1')
 	object_list.append(d)
 
 j = json.dumps(object_list)
