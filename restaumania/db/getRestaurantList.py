f6cdf6185d1e4009a6c0840ac1050f491bc0e078
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-22 11:59:10 +0200

    update

--- restaumania/db/getRestaurantList.py
+++ restaumania/db/getRestaurantList.py
@@ -1,12 +1,15 @@
 import MySQLdb
 import sys
 import json
+import collections
+
+host = "127.0.0.1"
+user = "root"
+passwd = "root"
+db = "restaumania"
 
 try:
-	db = MySQLdb.connect(host="127.0.0.1", 
-			     user="root", 
-			     passwd="root", 
-			     db="restaumania")
+	db = MySQLdb.connect(host, user, passwd, db)
 except:
 	print "connection error"
 	sys.exit(-1)
@@ -15,15 +18,16 @@ cursor = db.cursor()
 cursor.execute("SELECT restaurant_name, restaurant_description FROM restaurant")
 data = cursor.fetchall()
 
-rowarray_list = []
-
+object_list = []
 for single in data:
-	t = (single[0], single[1])
-	rowarray_list.append(t)
+	d = collections.OrderedDict()
+	d['rname'] = unicode(single[0], 'ISO-8859-1')
+	d['rdesc'] = unicode(single[1], 'ISO-8859-1')
+	object_list.append(d)
 
-j = json.dumps(rowarray_list)
-rowarrays_file = 'r_list.js'
-f = open(rowarrays_file, 'w')
+j = json.dumps(object_list)
+objects_file = 'restList.js'
+f = open(objects_file, 'w')
 print >> f, j
 
 db.close
