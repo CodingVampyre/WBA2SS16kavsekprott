165a3070769c9832aecf213da5726bd796a1296d
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-24 18:57:10 +0200

    add node-json2html, update fetchCard.js

--- restaumania/db/nodejs/fetchCard.js
+++ restaumania/db/nodejs/fetchCard.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var chalk = require('chalk');
+var json2html = require('node-json2html');
 
 fs.readFile(__dirname + "/restCard.json", function (err, data){
 	data = data.toString();
@@ -8,4 +9,9 @@ fs.readFile(__dirname + "/restCard.json", function (err, data){
 	for(var i=0; i<data.length; i++){
 		console.log(chalk.red(data[i].rname) + " (" + chalk.yellow(data[i].resid) + "): " + data[i].catname + " - " + data[i].scatname + ": " + data[i].price + " - " + data[i].entry + " (" + data[i].edetails + ")");
 	}
+
+	var transform = {'tag':'div','html':'${entry}: ${edetails}'};
+	var html = json2html.transform(data,transform);
+
+	console.log(html);
 });
