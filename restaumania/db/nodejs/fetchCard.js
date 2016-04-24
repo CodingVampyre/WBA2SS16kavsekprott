f451145282e23ffa7567c5ea072e09e2b23566b4
Lilly Kavsek <lillykavsek@hotmail.com>
2016-04-24 19:12:52 +0200

    update fetchCard.js

--- restaumania/db/nodejs/fetchCard.js
+++ restaumania/db/nodejs/fetchCard.js
@@ -6,12 +6,29 @@ fs.readFile(__dirname + "/restCard.json", function (err, data){
 	data = data.toString();
 	data = JSON.parse(data);
 
-	for(var i=0; i<data.length; i++){
-		console.log(chalk.red(data[i].rname) + " (" + chalk.yellow(data[i].resid) + "): " + data[i].catname + " - " + data[i].scatname + ": " + data[i].price + " - " + data[i].entry + " (" + data[i].edetails + ")");
-	}
+	/*for(var i=0; i<data.length; i++){
+		console.log(chalk.red(data[i].rname) 
+		+ " (" 
+		+ chalk.yellow(data[i].resid) 
+		+ "): " 
+		+ data[i].catname 
+		+ " - " 
+		+ data[i].scatname 
+		+ ": " 
+		+ data[i].price 
+		+ " - " 
+		+ data[i].entry 
+		+ " (" 
+		+ data[i].edetails 
+		+ ")");
+	}*/
 
-	var transform = {'tag':'div','html':'${entry}: ${edetails}'};
+	var transform = {'tag':'div','html':'<h2>${entry}</h2><p>${catname}/${scatname}</p><p class="carddetails">${edetails}</p><p class="cardprice">${price}</p>'};
 	var html = json2html.transform(data,transform);
 
 	console.log(html);
+	fs.writeFile('card.html', html, function (err) {
+		if (err) return console.log(err);
+		console.log("HTML done");
+	});
 });
